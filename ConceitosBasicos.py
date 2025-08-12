831099ca09b8b22c176bc535d4f22e2983690253
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-12 15:52:38 -0300

    LP - Adiciona alguns conceitos de python

---
+++ ConceitosBasicos.py
@@ -0,0 +1,26 @@
+if(2 > 1):
+    print("dois é maior que um")
+else:
+    print("dois é menor que um")
+    #Os espaços é essencial para lógica, pois identifica se as linhas de código pertencem a mesmo "Bloco"
+
+variavel1 = 1 #As variaveis identificam seus tipos automaticamente
+variavel2 = 'teste'
+variavel3 = 3.0
+variavelPreDefinida1 = str('Teste')
+variavelPreDefinida2 = int(2)
+variavelPreDefinida3 = float(2.5)
+variavelPreDefinida4 = bool(False) #Para definir o booleano deve se iniciar com letra Maiuscula: True/False
+declaracaoVariavelStr = "Teste"
+declaracaoVariavelStr = 'Teste' #uma String pode ser definida tanto com aspas simples ou aspas duplas
+declaracaoVariavelStr = 2 #Uma variavel pode receber qualquer tipo de dado ou seja: variaveis são reservatorios no qual voce pode guardar(atribuir) qualquer valor
+                          #variaveis podem ser diferenciadas por maiusculas e minusculas
+variavelComplex = 2J 
+
+
+print("variavel1 é:",type(variavel1)) #para descobrir o tipo da variavel, EXTRA: para juntar valores no mesmo print utiliza-se a virgula
+
+print(declaracaoVariavelStr)
+
+
+#NÃO GOSTEI DESSA LINGUAGEM FECHADAO COM JAVA
